26511b4af7d40ce1ef799cb8c1c0ddc74c9bb44e
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-06-20 23:14:54 +0200

    all up and running

--- sky/models.py
+++ sky/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Sky(models.Model):
+    name = models.CharField(max_length=256)
+    discription = models.TextField()
+    architect = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
